451092bd46af97e721395f1d9c01ed9c6ec78943
fmpberger88 <fabian.berger@protonmail.ch>
2024-07-25 07:45:36 +0200

    adding categoriyId to edit-category

--- src/routes.jsx
+++ src/routes.jsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
                 element: <CreateCategories />
             },
             {
-                path: 'edit-category',
+                path: 'edit-category/:categoryId',
                 element: <EditCategories />
             },
             {
