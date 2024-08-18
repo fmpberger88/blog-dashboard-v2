d6c6994deb9793e0c54834e5b6818add9644c250
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-18 18:09:38 +0200

    integrate menubar to tinymce

--- src/components/EditBlog/EditBlog.jsx
+++ src/components/EditBlog/EditBlog.jsx
@@ -146,16 +146,17 @@ const EditBlog = () => {
                         value={content}
                         init={{
                             height: 500,
-                            menubar: false,
+                            menubar: true,
                             plugins: [
                                 'advlist autolink lists link image charmap print preview anchor',
                                 'searchreplace visualblocks code fullscreen',
                                 'insertdatetime media table paste code help wordcount'
                             ],
                             toolbar:
-                                'undo redo | formatselect | bold italic backcolor | \
+                                'undo redo | formatselect fontselect fontsizeselect | bold italic backcolor | \
                                 alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | removeformat | help'
+                                bullist numlist outdent indent | removeformat | help',
+                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
                         }}
                         onEditorChange={handleEditorChange}
                     />
